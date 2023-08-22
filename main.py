cddcf1d8269c8eb46979b76282570e75703f3b93
QuaNilo <quanojo@gmail.com>
2023-08-22 16:34:10 +0100

    Changed to OOP and added twitch check user functionality

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ try:
     import googleapiclient.discovery
     import googleapiclient.errors
     import prawcore
+    import requests
+    from bs4 import BeautifulSoup
 
 except ModuleNotFoundError:
     print("Please download dependencies from requirements.txt")
@@ -62,7 +64,7 @@ class SocialMediaChecker:
             }
             return json_data
         else:
-            return None
+            return False
 
     def youtube_checker(self, handle):
         try:
@@ -83,12 +85,98 @@ class SocialMediaChecker:
                 }
                 return output
             else:
-                return jsonify({"available": True, "success": True})
+                return False
 
         except googleapiclient.errors.HttpError as e:
             print(f"An error occurred: {e}")
             return None
 
+    def tiktok_checker(self, handle):
+        pass
+        """session = requests.Session()
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-US",
+            "content-type": "application/json"
+        }
+        c = session.get(f'https://www.tiktok.com/@{handle}', headers=headers, timeout=60)
+        check = c.status_code
+        if c.status_code == 200:
+            soup = BeautifulSoup(c.content, "html.parser")
+            # Look for elements that indicate the existence of the account
+            account_not_found = soup.find("h1", text="This page isn't available.")
+            print(soup.text)
+            if account_not_found:
+                output = {
+                    'success': True,
+                    'is_available': True,
+                }
+                return output
+            else:
+                return False
+        else:
+            raise Exception("Unexpected error")"""
+
+    def twitch_checker(self,handle):
+        twitch = TwitchAPI(client_id='dd4yshe8vdg2j78mk6wqf3896ggn75', client_secret='qjm1y37x6840c60y3fb4qk1rf2sg9d')
+        response = twitch.check_user(handle)
+        if response.status_code:
+            output = {
+                'success': True,
+                'is_available': False,
+                'response': response.json()
+            }
+            return output
+        else:
+            return False
+
+class TwitchAPI:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def check_user(self,handle):
+        url = f'https://api.twitch.tv/helix/users?login={handle}'
+        headers = {
+            'Authorization': f'Bearer {os.environ.get("twitch_access_token")}',
+            'Client-ID': self.client_id
+        }
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 401:
+            self.getAccessToken()
+            headers['Authorization'] = f'Bearer {os.environ.get("twitch_access_token")}'
+
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                return response
+            else:
+                raise Exception(f"Something went wrong with request : {response}")
+
+        elif response.status_code == 200:
+            return response
+        else:
+            raise Exception(f"Unexpected Error occurred")
+
+
+    def getAccessToken(self):
+        tokenResponse = requests.post(url='https://id.twitch.tv/oauth2/token',
+                                      data={'client_id': self.client_id,
+                                               'client_secret': self.client_secret,
+                                               'grant_type': 'client_credentials'})
+        if tokenResponse.status_code == 200:
+            response_data = tokenResponse.json()  # Get the JSON response content
+            access_token = response_data.get('access_token')
+
+            if access_token:
+                os.environ['twitch_access_token'] = access_token
+            else:
+                print("Access token not found in response.")
+                return jsonify('FAILED TO GET ACCESS_TOKEN'), 500
+        else:
+            print("Token request failed with status code:", tokenResponse.status_code)
+            return jsonify("Token request failed with status code:", tokenResponse.status_code), 500
 
 
 @app.route('/check_handle', methods=['GET'])
@@ -105,12 +193,28 @@ def check_handle():
         response_data = {"success": "false", "error": "social_network not provided"}
         return jsonify(response_data), 400
 
+
     if social_network == "reddit":
         response = social_media_checker.reddit_checker(handle)
         if response:
             return response, 200
         else:
             return jsonify({"available": "true", "success": "true"})
+
+
+    if social_network == 'tiktok':
+        response = social_media_checker.tiktok_checker(handle)
+        if response:
+            return response, 200
+        else:
+            return jsonify({'success': False, 'error': "Unexpected error occurred"})
+
+    if social_network == 'twitch':
+        response = social_media_checker.twitch_checker(handle)
+        if response:
+            return response, 200
+        else:
+            return jsonify({'success': False, 'error': "Unexpected error occurred"})
 
 
     if social_network == "youtube":
